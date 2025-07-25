15594538377ebbeac89e5e2bc017e5456bd5a3dc
David Collom <david@collom.co.uk>
2025-07-25 09:57:40 +0100

    Adding getting CRD's

---
+++ pkg/k8s/crds.go
@@ -0,0 +1,28 @@
+package k8s
+
+import (
+	"testing"
+
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+	apixv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apixclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/stretchr/testify/require"
+)
+
+func GetCustomResourceDefinition(t *testing.T, options *terrak8s.KubectlOptions, crdName string) *apixv1.CustomResourceDefinition {
+	t.Helper()
+
+	crd, err := GetCustomResourceDefinitionE(t, options, crdName)
+	require.NoError(t, err)
+	return crd
+}
+
+func GetCustomResourceDefinitionE(t *testing.T, options *terrak8s.KubectlOptions, crdName string) (*apixv1.CustomResourceDefinition, error) {
+	client, err := apixclientset.NewForConfig(options.RestConfig)
+	if err != nil {
+		return nil, err
+	}
+	return client.ApiextensionsV1().CustomResourceDefinitions().Get(t.Context(), crdName, metav1.GetOptions{})
+}
